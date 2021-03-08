decb117e62b6b25cff41ced587ea33cdbb3f8777
maxymilien <62253138+maxymilien@users.noreply.github.com>
2021-03-08 10:54:11 +0100

    correction des différents points abordés lors de la réunion+ nouveau résultat

--- taskJsonToExcel.py
+++ taskJsonToExcel.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 import configFichier
+import dateparser
 """
 initialisation de variables
 """
@@ -16,8 +17,8 @@ card = {}
 """
 ouverture du fichier json 
 """
-with open(configFichier.filejson, "r") as file:
-  trello = configFichier.json.load(file)
+with open(configFichier.filejson, "r",encoding="utf-8") as file:
+ trello = configFichier.json.load(file)
 listecards = trello["cards"]
 listelistes= trello["lists"]
 listemembres = trello["members"]
@@ -33,25 +34,47 @@ try:
     lab = ""
     mem = ""
     #mettre les infos direct de la carte dans le dico
-    card["A"] = carte["name"]
-    card["E"] = carte["desc"]
-    card["M"] = carte["due"]
+    card['B'] = carte["name"]
+    card['C'] = carte["desc"]
+    d=dateparser.parse(str(carte["due"]))
+    if d is None:
+        card['D'] = ""
+    else:
+       card['D'] = str(d.day)+"/"+str(d.month)+"/"+str(d.year)
     for label in carte["labels"]:
-           lab = str(lab)+","+str(label["name"])
-    card["O"] =lab
+           if lab == "":
+               lab = str(label["name"])
+           else:
+               lab = str(lab)+","+str(label["name"])
+    card['E'] =lab
     #chercher les id des différentes donnée externe à la carte
     for liste in listelistes:
         if carte["idList"] == liste["id"]:
-            card["S"] = liste["name"]
+            card['A'] = liste["name"]
     for membre in listemembres:
         for id in carte["idMembers"]:
              if id == membre["id"]:
                membreCard.append(membre["initials"])
     for item in membreCard:
-        mem = str(mem)+","+str(item)
-    card['V'] = mem
+        if mem == "":
+            mem = str(item)
+        else:
+         mem = str(mem)+","+str(item)
+    card['F'] = mem
     print(card)
+    ws.column_dimensions['A'].width = 25
+    ws["A1"] = "liste"
+    ws.column_dimensions['B'].width = 65
+    ws["B1"] = "nom tâche"
+    ws.column_dimensions['C'].width = 140
+    ws["C1"] = "description"
+    ws.column_dimensions['D'].width = 25
+    ws["D1"] = "echéance"
+    ws.column_dimensions['E'].width = 35
+    ws["E1"] = "étiquettes"
+    ws["F1"] = "équipe"
     ws.append(card)
     card.clear()
 finally:
- wb.save(filename=configFichier.filexlsx)
+    print("fin")
+    wb.save(filename=configFichier.filexlsx)
